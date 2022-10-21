b9c0df87d78ef1ac6483c418a945ec604a972fc1
krispis1 <belickaskristupas@outlook.com>
2022-10-21 11:54:37 +0300

    add custom width box

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const density = "@QB#NgWM8RDHdOKq9$6khEPXwmeZaoS2yjufF]}{tx1zv7lciL/\\|?*>r^;:_\"~,'.-`";
+var asciiImage = "";
+
+const clearAscii = () => {
+    asciiImage = "";
+    const p = document.getElementById('ascii');
+    p.innerHTML = "";
+}
+
+const clearAndReload = () => {
+    clearAscii();
+    location.reload();
+}
 
 const loadFile = (event) => {
-    var asciiImage = "";
-    var canvas = document.getElementById('output');
-    let image = new Image();
+    clearAscii();
+
+    const canvas = document.getElementById('output');
+    const widthInput = document.getElementById('width');
+    const image = new Image();
     image.src = URL.createObjectURL(event.target.files[0]);
-    var context = canvas.getContext("2d", {willReadFrequently: true});
+    const context = canvas.getContext("2d", { willReadFrequently: true });
     image.onload = () => {
-        const asciiWidth = 90;
-        const asciiHeight = Math.floor((asciiWidth*100/image.width) * image.height / 100 * 0.45);
+
+        var asciiWidth;
+        if (widthInput.value != "" && !isNaN(widthInput.value)) {
+            asciiWidth = Math.floor(parseInt(widthInput.value, 10));
+        } else {
+            asciiWidth = 90;
+        }
+        const asciiHeight = Math.floor((asciiWidth * 100 / image.width) * image.height / 100 * 0.45);
 
         context.drawImage(image, 0, 0, asciiWidth, asciiHeight);
 
@@ -30,7 +50,7 @@ const loadFile = (event) => {
                 const green = data[i + 1];
                 const blue = data[i + 2];
 
-                const brightness = parseInt(0.2126*red + 0.7152*green + 0.0722*blue, 10);
+                const brightness = parseInt(0.2126 * red + 0.7152 * green + 0.0722 * blue, 10);
                 const brightnessPercent = brightness * 100 / 255;
                 const asciiCalc = density.length * brightnessPercent / 100;
                 const asciiMatch = parseInt(asciiCalc, 10);
@@ -41,13 +61,15 @@ const loadFile = (event) => {
                     asciiImage += density[0];
                 }
 
-                if ((i + 4) % (asciiWidth*4) == 0) {
+                if ((i + 4) % (asciiWidth * 4) == 0) {
                     asciiImage += "\n";
                 }
 
                 if ((i + 4) >= data.length) {
-                    var p = document.getElementById('ascii');
+                    const p = document.getElementById('ascii');
                     p.innerHTML = asciiImage;
+                    const f = document.getElementById('file');
+                    f.value = "";
                 }
             }
         }
