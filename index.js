cfe59c66af515bf1b4727cb295992fa3dcd4297c
krispis1 <belickaskristupas@outlook.com>
2022-10-21 12:56:32 +0300

    add webcam functionality

--- index.js
+++ index.js
@@ -75,3 +75,71 @@ const loadFile = (event) => {
         }
     }
 };
+
+var video = document.getElementById("webcam");
+
+const asciiVideo = () => {
+    clearAscii();
+
+    const canvas = document.getElementById('output');
+    const widthInput = document.getElementById('width');
+    const context = canvas.getContext("2d", { willReadFrequently: true });
+    var asciiWidth;
+    if (widthInput.value != "" && !isNaN(widthInput.value)) {
+        asciiWidth = Math.floor(parseInt(widthInput.value, 10));
+    } else {
+        asciiWidth = 90;
+    }
+    const asciiHeight = Math.floor((asciiWidth * 100 / video.videoWidth) * video.videoHeight / 100 * 0.45);
+
+    context.drawImage(video, 0, 0, asciiWidth, asciiHeight);
+
+    const imgData = context.getImageData(0, 0, asciiWidth, asciiHeight);
+    const data = imgData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+        const red = data[i];
+        const green = data[i + 1];
+        const blue = data[i + 2];
+
+        const brightness = parseInt(0.2126 * red + 0.7152 * green + 0.0722 * blue, 10);
+        const brightnessPercent = brightness * 100 / 255;
+        const asciiCalc = density.length * brightnessPercent / 100;
+        const asciiMatch = parseInt(asciiCalc, 10);
+
+        if (asciiMatch != 0) {
+            asciiImage += density[asciiMatch - 1];
+        } else {
+            asciiImage += density[0];
+        }
+
+        if ((i + 4) % (asciiWidth * 4) == 0) {
+            asciiImage += "\n";
+        }
+
+        if ((i + 4) >= data.length) {
+            const p = document.getElementById('ascii');
+            p.innerHTML = asciiImage;
+        }
+    }
+
+};
+
+var startDrawingASCIIVideo = function () {
+    var fps = 30;
+    setInterval(asciiVideo, Math.round(1000 / fps));
+}
+
+const startVideo = () => {
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+                video.srcObject = stream;
+                startDrawingASCIIVideo();
+                console.log("video started");
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+}
